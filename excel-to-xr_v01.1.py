ae03fb5a61e9f46bf609ce05b24741e228d7dcc0
leonardo.anez@gmail.com <Paralelepiped0>
2018-08-07 14:01:16 -0400

    Nueva version

--- excel-to-xr_v01.1.py
+++ excel-to-xr_v01.1.py
@@ -1,8 +1,9 @@
 from xlrd import open_workbook
 
+
 class Interface(object):
     def __init__(self, old_int, new_int, neg, bd, dot1q, descrip, mtu,
-     vrf, xc, qos_in, qos_out, ip1, ip2, ip3, ip4):
+     vrf, xc_peer, xc_vcid, qos_in, qos_out, ip1, ip2, ip3, ip4):
         self.old_int = old_int
         self.new_int = new_int
         self.neg = neg
@@ -11,7 +12,8 @@ class Interface(object):
         self.descrip = descrip
         self.mtu = mtu
         self.vrf = vrf
-        self.xc = xc
+        self.xc_peer = xc_peer
+        self.xc_vcid = xc_vcid
         self.qos_in = qos_in
         self.qos_out = qos_out
         self.ip1 = ip1
@@ -28,21 +30,22 @@ class Interface(object):
                "  descrip: {5}\n"
                "  mtu: {6}\n"
                "  vrf: {7}\n"
-               "  xc: {8}\n"
-               "  qos_in: {9}\n"
-               "  qos_out: {10}\n"
-               "  ip1: {11}\n"
-               "  ip2: {12}\n"
-               "  ip3: {13}\n"
-               "  ip4: {14}\n"
+               "  xc_peer: {8}\n"
+               "  xc_vcid: {9}\n"
+               "  qos_in: {10}\n"
+               "  qos_out: {11}\n"
+               "  ip1: {12}\n"
+               "  ip2: {13}\n"
+               "  ip3: {14}\n"
+               "  ip4: {15}\n"
                .format(self.old_int, self.new_int, self.neg, self.bd, self.dot1q,
-               self.descrip, self.mtu, self.vrf, self.xc, self.qos_in, self.qos_out, 
+               self.descrip, self.mtu, self.vrf, self.xc_peer, self.xc_vcid, self.qos_in, self.qos_out,
                self.ip1, self.ip2, self.ip3, self.ip4))
 
 # Load in the workbook
-wb = open_workbook('Resumen Servicios MSC.XLSM')
+wb = open_workbook('UIOCNDE01_test.xlsx')
 sheet_names = wb.sheet_names()
-sheet = wb.sheet_by_name(sheet_names[3])
+sheet = wb.sheet_by_name(sheet_names[0])
 
 number_of_rows =  sheet.nrows
 number_of_columns = sheet.ncols 
@@ -62,7 +65,7 @@ for row in range(1, number_of_rows):
     item = Interface(*values)
     items.append(item)
 
-file = open('CNT.yml', 'a+') 
+file = open('prov-service-model/deploy-model.yml', 'a+')
 file.write('---\n')
 file.write('interfaces:\n')
 for item in items:
@@ -70,5 +73,3 @@ for item in items:
     file.write(str(item))
 
 file.close()
-
-
